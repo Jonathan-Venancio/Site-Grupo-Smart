f72479e56f3ac0bc2d992d0c71d95c1ac784813d
Jonathan Venancio <jonathan.ghabriel@gmail.com>
2024-06-25 21:36:31 +0100

    adicionando as paginas principais do site

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', homepage, name="homepage")
+    path('', homepage, name="homepage"),
+    path('lumiar/', lumiar, name="lumiar"),
+    path('olivais/', olivais, name="olivais"),
+    path('alvalade/', alvalade, name="alvalade"),
+    path('minhaconta/', minha_conta, name="minha_conta"),
+    path('login/', login, name="login"),
+    path('carrinho/', carrinho, name="carrinho"),
+    path('checkout/', checkout, name="checkout")
 ]
