8b563f8baca740ca03ca5a481bb7c8a5f1a2d39e
Jonathan Venancio <jonathan.ghabriel@gmail.com>
2024-07-01 21:47:19 +0100

    Update models.py

--- ecommerce/loja/models.py
+++ ecommerce/loja/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+#cliente
+    #nome
+    #email
+    #telefone
+    #usuario
+
+#produto
+    #imagem
+    #nome
+    #preço
+    #ativo
+    #categoria
+    #tipo
+
+#categorias (HP, Epson, Canon, Brother, Material de escritório, Gaming, Computadores)
+    #nome
+
+
+#tipos (Compativeis, originais, toners, )
+    #nome
+
+#pedidos
+    #
